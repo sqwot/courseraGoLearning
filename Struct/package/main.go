0ef7881d63aa0969460ae3e18bbfa6762d80d645
sqwot <sqwot@mail.ru>
2021-05-07 12:27:21 +0300

    init 2

--- Struct/package/main.go
+++ Struct/package/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"coursera/Struct/package/person"
 	"fmt"
-	"D:/golang/Coursera/Struct/package/person"
 )
 
 func main() {
 	fmt.Println("***********Package**********")
 	p := person.NewPerson(1, "rvasily", "secret")
-	fmt.Printf("main.PrintPerson: %+v\n", p.secret)
+	//fmt.Printf("main.PrintPerson: %+v\n", p.secret)
 	secret := person.GetSecret(p)
+	fmt.Println("GetSecret", secret)
 	fmt.Println("GetSecret", secret)
 }
