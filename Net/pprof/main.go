0967caf97487f9b81f3b3cd979d65d4ed8cae4aa
sqwot <sqwot@mail.ru>
2021-05-23 12:56:36 +0300

    add pprof http

---
+++ Net/pprof/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+)
+
+type Post struct {
+	ID       int
+	Text     string
+	Author   string
+	Comments int
+	Time     time.Time
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	s := ""
+	for i := 0; i < 1000; i++ {
+		p := &Post{ID: i, Text: "new post"}
+		s += fmt.Sprint("%#v", p)
+	}
+	w.Write([]byte(s))
+}
+
+func main() {
+	colorReset := "\033[0m"
+	colorGreen := "\033[32m"
+	fmt.Println(string(colorGreen), "***************PPROF***************")
+	fmt.Println(string(colorReset), "")
+
+	http.HandleFunc("/", handle)
+
+	fmt.Println("Server started at :8080")
+	http.ListenAndServe(":8080", nil)
+}
